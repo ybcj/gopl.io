35141defaf1bfbc6ca4050a9b9bb42185a1bddf2
ybcj <ybcj_lv@163.com>
2020-02-08 22:36:13 +0800

    读取命令行参数

--- ch1/echo3.go
+++ ch1/echo3.go
@@ -9,4 +9,8 @@ import (
 func main() {
 	fmt.Println(strings.Join(os.Args[1:], " "))
 	fmt.Println(os.Args[1:])
+	fmt.Println(os.Args[0])
+	for i, arg := range os.Args {
+		fmt.Printf("index %d， value %s\n", i, arg)
+	}
 }
