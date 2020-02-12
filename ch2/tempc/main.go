e34586e20838c2cf2c0043f9f9223382aea70508
ybcj <ybcj_lv@163.com>
2020-02-13 00:10:25 +0800

    包导入

--- ch2/tempc/main.go
+++ ch2/tempc/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	fmt.Printf("Brrrr! %v\n", tempconv.AbsoluteZeroC)
 	fmt.Println(tempconv.CToF(tempconv.BoilingC))
 	fmt.Println(tempconv.FToC(tempconv.CToF(tempconv.AbsoluteZeroC)))
 }
