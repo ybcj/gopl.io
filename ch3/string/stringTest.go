a66c6c38409f015c292d979587957f98e4bc2557
ybcj <ybcj_lv@163.com>
2020-03-02 16:30:44 +0800

    unicode

---
+++ ch3/string/stringTest.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	s := "Hello, 世界"
+	fmt.Println(len(s))
+	fmt.Println(utf8.RuneCountInString(s))
+
+	for i := 0; i < len(s); {
+		r, size := utf8.DecodeRuneInString(s[i:])
+		fmt.Printf("%d\t%c\n", i, r)
+		fmt.Println(size, r)
+		i += size
+	}
+	fmt.Println(string(1234567))
+}
